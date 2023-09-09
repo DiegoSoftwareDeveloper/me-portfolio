8b24cdd86c575cfc2326ef3b751940f92e667165
DiegoSoftwareDeveloper <diegobarreto702@gmail.com>
2023-09-08 19:56:28 -0500

    feat: Add change theme color

---
+++ assets/js/frameworks/theme-toggle.js
@@ -0,0 +1,62 @@
+const storageKey = 'light'
+
+const onClick = () => {
+  // flip current value
+  theme.value = theme.value === 'light'
+    ? 'dark'
+    : 'light'
+
+  setPreference()
+}
+
+const getColorPreference = () => {
+  if (localStorage.getItem(storageKey))
+    return localStorage.getItem(storageKey)
+  else
+    return window.matchMedia('(prefers-color-scheme: light)').matches
+      ? 'dark'
+      : 'light'
+}
+
+const setPreference = () => {
+  localStorage.setItem(storageKey, theme.value)
+  reflectPreference()
+}
+
+const reflectPreference = () => {
+  document.firstElementChild
+    .setAttribute('data-theme', theme.value)
+
+  document
+    .querySelector('#theme-toggle')
+    ?.setAttribute('aria-label', theme.value)
+
+  if(theme.value !== 'light'){
+    document.documentElement.style.setProperty('--bg-color', '#FFFFFF');
+    document.documentElement.style.setProperty('--text-color', '#1f242d');
+    document.documentElement.style.setProperty('--second-bg-color', '#D1D1D1');
+    document.documentElement.style.setProperty('--color-icon', '#323946');
+  }else{
+    document.documentElement.style.setProperty('--bg-color', '#1f242d');
+    document.documentElement.style.setProperty('--text-color', '#FFFFFF');
+    document.documentElement.style.setProperty('--second-bg-color', '#323946');
+    document.documentElement.style.setProperty('--color-icon', 'hsl(220, 9%, 94%)');
+  }
+}
+
+const theme = {
+  value: getColorPreference(),
+}
+
+// set early so no page flashes / CSS is made aware
+reflectPreference()
+
+window.onload = () => {
+  // set on load so screen readers can see latest value on the button
+  reflectPreference()
+
+  // now this script can find and listen for clicks on the control
+  document
+    .querySelector('#theme-toggle')
+    .addEventListener('click', onClick)
+}
--- assets/js/styles.js
+++ assets/js/styles.js
@@ -1,55 +1,79 @@
 /**
- * Action responsive navbar menu toggle 
+ * Action responsive navbar menu toggle
  */
-let menuIcon = document.querySelector('#menu-icon')
-let navbar = document.querySelector('.navbar')
+let menuIcon = document.querySelector("#menu-icon");
+let navbar = document.querySelector(".navbar");
 
 menuIcon.onclick = () => {
-    menuIcon.classList.toggle('bx-x')
-    navbar.classList.toggle('active')
-}
+  menuIcon.classList.toggle("bx-x");
+  navbar.classList.toggle("active");
+};
 
 /**
  * Action navbar sticky on scroll / add class active in section view / remove responsive navbar menu (hamburguer)
  */
-let sections = document.querySelectorAll('section')
-let navLinks = document.querySelectorAll('header nav a')
+let sections = document.querySelectorAll("section");
+let navLinks = document.querySelectorAll("header nav a");
 
 window.onscroll = () => {
-    sections.forEach(sec => {
-        let top = window.scrollY
-        let offset = sec.offsetTop
-        let height = sec.offsetHeight
-        let id = sec.getAttribute('id')
+  sections.forEach((sec) => {
+    let top = window.scrollY;
+    let offset = sec.offsetTop;
+    let height = sec.offsetHeight;
+    let id = sec.getAttribute("id");
 
-        if (top >= offset && top < offset + height) {
-            navLinks.forEach(links => {
-                links.classList.remove('active')
-                document.querySelector('header nav a[href*=' + id + ']').classList.add('active')
-            })
-        }
-    })
+    if (top >= offset && top < offset + height) {
+      navLinks.forEach((links) => {
+        links.classList.remove("active");
+        document
+          .querySelector("header nav a[href*=" + id + "]")
+          .classList.add("active");
+      });
+    }
+  });
 
-    let header = document.querySelector('header')
+  let header = document.querySelector("header");
 
-    header.classList.toggle('sticky', window.scrollY + 100)
+  header.classList.toggle("sticky", window.scrollY + 100);
 
-    /**
-     * Remove class hamburguer menu 
-     */
-    menuIcon.classList.remove('bx-x')
-    navbar.classList.remove('active')
-}
+  /**
+   * Remove class hamburguer menu
+   */
+  menuIcon.classList.remove("bx-x");
+  navbar.classList.remove("active");
+};
 
 /**
  * Actions Scroll Reveal
  */
-/*
-window.sr = ScrollReveal({ 
-    reset: true,
-    distance: '80px',
-    duration: 2000,
-    delay: 200
-})
+// Changing the defaults
+window.sr = ScrollReveal({
+  //reset: true,
+  distance: "80px",
+  duration: 2000,
+  delay: 200,
+});
 
-sr.reveal('.home-content, .heading', { origin: 'top' })*/
+// Customizing a reveal set
+sr.reveal(".home-content, .heading", { origin: "top" });
+sr.reveal(".home-img, .services-container, .portfolio-box, .contact form", {
+  origin: "bottom",
+});
+sr.reveal(".home-content h1, .about-img", { origin: "left" });
+sr.reveal(".home-content p, .about-content", { origin: "right" });
+
+/**
+ * Actions Typed.js
+ */
+let typed = new Typed(".multiple-text", {
+  strings: [
+    ".Net Developer",
+    "SQL Server Developer",
+    "Add-ons Developer",
+    "Web Developer",
+  ],
+  typeSpeed: 100,
+  backSpeed: 100,
+  backDelay: 1000,
+  loop: true,
+});
